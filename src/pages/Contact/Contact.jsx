092a9c431f8ae4d5407617df05c4e4ea0df7b845
Steve Kaats <stevenakaats@gmail.com>
2025-05-29 17:33:37 -0700

    added contact info

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,10 @@ export default function Contact() {
   return (
     <>
       <h1>Contact</h1>
+      <h3>Feel free to reach out for collaboration, questions, or just want to say hi!</h3>
+      <ul>
+        <a href="https://linkedin.com/in/stevekaats" target="_blank" rel="noopener noreferrer"><li><b>LinkedIn</b></li></a>
+      </ul>
     </>
   )
 }
