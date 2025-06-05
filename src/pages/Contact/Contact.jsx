8be487b578a7f686e17e7283b23741c0ca6839d6
Steve Kaats <stevenakaats@gmail.com>
2025-06-05 14:48:07 -0700

    Formatted contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,7 +12,7 @@ export default function Contact() {
         Feel free to reach out for collaboration, questions, or just want to say
         hi!
       </h3>
-      <p>Connect with me:</p>
+      <p>Connect with me on:</p>
       <div className="logo">
         <a
           href="https://linkedin.com/in/stevekaats"
@@ -22,6 +22,7 @@ export default function Contact() {
           <img src={linkedin} alt="li logo" style={{ height: "32px" }} />
         </a>
       </div>
+      <h3><u>Contact me form:</u></h3>
       <div className="contactContainer">
         <ContactForm />
       </div>
